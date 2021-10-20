76564d59e562e28459cc6b31fa7e23ee4d502d7f
Zach Brady <zacbra@gmail.com>
2021-10-20 15:16:04 -0400

    Add mw for CORS to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => {
     console.log("server on 5000");

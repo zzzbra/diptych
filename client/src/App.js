3f45467d10cf8e1b383746dacdddfeb38491e00d
Zach Brady <zacbra@gmail.com>
2021-10-20 19:29:29 -0400

    Add some styles to the Input

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import InputTodo from './components/InputTodo';
+
 function App() {
   return (
-    <>
-        Inside fragment
-    </>
+    <div className="max-w-lg mx-auto py-10">
+        <InputTodo />
+    </div>
   );
 }
 
--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const  InputTodo = () => (
     <div>
-        <h1>Input Todo</h1>
+         <form>
+             <div className="mb-6">
+                <label for="todo-input" className="text-sm font-medium text-gray-900 block mb-2">Your next todo</label>
+                <input
+                    type="text"
+                    id="todo-input"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                />
+            </div>
+             <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add</button>
+         </form>
     </div>
 );
 

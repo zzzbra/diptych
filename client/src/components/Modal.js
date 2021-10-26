cc40ce107d6fce2b3e22868d81095dc99c39811f
Zach Brady <zacbra@gmail.com>
2021-10-26 16:24:37 -0400

    Remove unused import from React

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useRef } from 'react';
+import { Fragment, useRef } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import Input from './Input';
 

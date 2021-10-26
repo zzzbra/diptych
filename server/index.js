2b6723425471cf2dfe19f795ce3f78593ee47862
Zach Brady <zacbra@gmail.com>
2021-10-26 15:29:27 -0400

    Add morgan logger to server

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 const pool = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(morgan('dev')); // predefined: combined, common, dev, short, tiny
 app.use(express.json()); // req.body
 
 // Routes

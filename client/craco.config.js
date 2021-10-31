ae0e60cedf5fb33341881116bcd0f22919b6f146
Zach Brady <zacbra@gmail.com>
2021-10-31 13:51:55 -0400

    Add placeholder for absolute paths

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,10 +1,22 @@
+// const path = require('path');
+
 module.exports = {
+  // TODO - debug following this guide: https://dev.to/bybruno/configuring-absolute-paths-in-react-for-web-without-ejecting-en-us-52h6
+  // webpack: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src/'),
+  //   },
+  // },
+  // jest: {
+  //   configure: {
+  //     moduleNameMapper: {
+  //       '^@(.*)$': '<rootDir>/src$1',
+  //     },
+  //   },
+  // },
   style: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
-}
+};

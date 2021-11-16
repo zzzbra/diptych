d17a241104b46ae3da014f89c93f763719a312ba
Zach Brady <zacbra@gmail.com>
2021-11-16 12:51:14 -0500

    Migrate enroll call to knex

--- server/routes/enrollments.js
+++ server/routes/enrollments.js
@@ -2,19 +2,22 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
-const pool = require('../db');
+const db = require('../db');
 const authorization = require('../middleware/auth');
 const { camelCaseKeys, snakeCaseKeys } = require('../utils/formatting');
 
 router.post('/enroll', authorization, async (req, res) => {
   const { course_id } = snakeCaseKeys(req.body);
   try {
-    const newEnrollment = await pool.query(
-      'INSERT INTO enrollments (user_id, course_id) VALUES ($1, $2) RETURNING *',
-      [req.user, course_id],
+    const [newEnrollment] = await db('enrollments').insert(
+      {
+        user_id: req.user,
+        course_id,
+      },
+      ['*'],
     );
 
-    res.json(camelCaseKeys(newEnrollment.rows[0]));
+    res.json(camelCaseKeys(newEnrollment));
   } catch (error) {
     console.log(error.message);
     res.json(error.message);

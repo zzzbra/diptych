9e1f353af92b79eb698a90ea8f596e9a83b4fbc9
Zach Brady <zacbra@gmail.com>
2021-11-16 12:17:47 -0500

    Set NODE_ENV

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
-console.log('NODE_ENV', process.env.NODE_ENV);
-
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 };

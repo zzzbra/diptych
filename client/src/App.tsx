bf230ccac2129663c325d420436e44b0d5b4a6ed
Zach Brady <zacbra@gmail.com>
2021-11-19 16:20:27 -0500

    Create distinct Dashboards for teachers & students

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { UnauthOnlyRoute, PrivateRoute } from 'views/PrivateRoute';
-import Dashboard from 'views/Dashboard/index';
+import TeacherDashboard from 'views/Dashboard/TeacherDashboard';
+import StudentDashboard from 'views/Dashboard/StudentDashboard';
 import Login from 'views/Login';
 import Signup from 'views/Signup';
 import TeacherRegistrar from 'views/Registrar/TeacherView';
@@ -32,21 +33,30 @@ function App() {
             <Signup />
           </UnauthOnlyRoute>
 
-          <PrivateRoute exact path="/dashboard">
-            <Dashboard />
-          </PrivateRoute>
+          <PrivateRoute
+            exact
+            path="/dashboard"
+            render={(props: any) =>
+              user?.userIsTeacher ? (
+                <TeacherDashboard {...props} />
+              ) : (
+                <StudentDashboard {...props} />
+              )
+            }
+          />
 
           <PrivateRoute
             exact
             path="/registrar"
-            render={(props: any) => {
-              return user?.userIsTeacher ? (
+            render={(props: any) =>
+              user?.userIsTeacher ? (
                 <TeacherRegistrar {...props} />
               ) : (
                 <StudentRegistrar {...props} />
-              );
-            }}
+              )
+            }
           />
+
           <Route path="*" component={() => <h1>Uh Oh! Page not found!!</h1>} />
         </Switch>
       </PageContentWrapper>
---
+++ client/src/views/Dashboard/TeacherDashboard.tsx
@@ -0,0 +1,18 @@
+import Link from 'components/Link';
+import React from 'react';
+
+import TeachersStudents from './TeachersStudents';
+
+const Dashboard = (props: any) => {
+  return (
+    <div className="py-8">
+      <h1 className="pb-4">Welcome to MOOC-SRS</h1>
+      <TeachersStudents />
+
+      <h3 className="pb-4">More</h3>
+      <Link to="/registrar">Manage your curriculum</Link>
+    </div>
+  );
+};
+
+export default Dashboard;

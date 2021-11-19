48690ad745177cfdd400f4d03e07a07cf9ab09e9
Zach Brady <zacbra@gmail.com>
2021-11-19 16:20:27 -0500

    Create distinct Dashboards for teachers & students

---
+++ client/src/views/Dashboard/StudentDashboard.tsx
@@ -0,0 +1,21 @@
+import Link from 'components/Link';
+import React from 'react';
+
+import StudentsClasses from './StudentsClasses';
+
+const Dashboard = (props: any) => {
+  return (
+    <div className="py-8">
+      <h1 className="pb-4">Welcome to MOOC-SRS</h1>
+      <StudentsClasses />
+
+      <h2>TODO: show what reviews are ready</h2>
+      <h2>TODO: show when a lesson is ready</h2>
+
+      <h3 className="py-4 border-t-2">More</h3>
+      <Link to="/registrar">Add/Drop Classes</Link>
+    </div>
+  );
+};
+
+export default Dashboard;

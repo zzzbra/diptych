5750b584a28d6f87b608fa9ff1a8ba713e9abda5
Zach Brady <zacbra@gmail.com>
2021-12-02 18:17:16 -0500

    Style ErrorMessage

--- client/src/components/ErrorMessage.tsx
+++ client/src/components/ErrorMessage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ExclamationCircleIcon } from '@heroicons/react/outline';
 
 const ErrorMessage = ({ error }: any) => {
   console.error(error);
   return (
-    <div>
-      <h3 className="text-red-600">Something went wrong.</h3>
+    <div className="border-2 border-red-600 p-4 flex flex-row items-middle">
+      <ExclamationCircleIcon className="text-red-600 w-6 mr-2" />
+      <h3 className="text-red-600 pb-0 inline">Something went wrong.</h3>
     </div>
   );
 };

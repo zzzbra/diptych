8df2db52af834d3423469bb73abc2ffa59ab0f5a
Zach Brady <zacbra@gmail.com>
2021-12-07 17:52:22 -0500

    Adjust css classes on Button as as to keep tree shaking

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -3,31 +3,37 @@ import cn from 'classnames';
 
 // import TailwindColors from 'tailwindcss/colors';
 
-type ButtonTypes = "button" | "submit" | "reset" | undefined;
+type ButtonTypes = 'button' | 'submit' | 'reset' | undefined;
 
 interface ButtonProps {
   // TODO: figure this out
   // color?: typeof TailwindColors,
-  color?: string,
-  children: any,
-  className?: string,
-  onClick?: any, // Optional, as in case of form submit button
-  type?: ButtonTypes,
+  color?: string;
+  children: any;
+  className?: string;
+  onClick?: any; // Optional, as in case of form submit button
+  type?: ButtonTypes;
 }
 
 const Button = ({
   children,
   className,
   onClick,
-  color = "blue",
-  type = "button",
-}: ButtonProps ) => {
+  color = 'blue',
+  type = 'button',
+}: ButtonProps) => {
   return (
     <button
       {...{
         className: cn(
-          `p-2 bg-${color}-700 rounded-lg border text-white hover:bg-${color}-800 font-medium text-sm px-5 py-2.5 text-center`,
+          `p-2 rounded-lg border text-white font-medium text-sm px-5 py-2.5 text-center`,
           className,
+          {
+            'bg-blue-700 hover:bg-blue-800': color === 'blue',
+            'bg-red-700 hover:bg-rec-800': color === 'red',
+            'bg-purple-700 hover:bg-purple-800': color === 'purple',
+            'bg-green-700 hover:bg-green-800': color === 'green',
+          },
         ),
         onClick,
         type,
